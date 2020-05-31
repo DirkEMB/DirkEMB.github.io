66e481afa78b1c22c3931ee19e751b3ed296cbc7
Dirk Bogaert <dirk@bogaert.es>
2020-05-31 17:18:17 +0200

    Update script.js

--- home/js/script.js
+++ home/js/script.js
@@ -7,7 +7,10 @@ window.addEventListener('load', (event) => {
 });
 createWeatherFrame = function(link) {
   var newEl = document.createElement("iframe");
+  var mySource = "https://www.meteoblue.com/en/weather/widget/daily/";
+  mySource += "barcelona_spain_3128760";
+  mySource += "?geoloc=fixed&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&winddirection=1&uv=0&humidity=0&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&pressure=0&layout=light";
   newEl.classList.add("weatherFrame");
-  newEl.src="https://www.google.com";
+  newEl.src=mySource;
   weatherDiv.appendChild(newEl);
 };
