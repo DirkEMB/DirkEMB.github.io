ba41002c56292f32993e21037985064882efaece
Dirk Bogaert <dirk@bogaert.es>
2020-05-31 16:48:54 +0200

    Update script.js

--- home/js/script.js
+++ home/js/script.js
@@ -1,4 +1,6 @@
-console.log("hellow");
+
 window.addEventListener('load', (event) => {
   console.log('page is fully loaded');
+  set weatherDiv = document.querySelector("weather");
+  weatherDiv.appendChild("div");
 });
