64e8aec8dd68ed6721bbc18b976ead39234849ee
Dirk Bogaert <dirk@bogaert.es>
2020-06-04 18:02:59 +0200

    Update script.js

--- home/js/script.js
+++ home/js/script.js
@@ -10,8 +10,18 @@ var weatherList = [
   "amsterdam_netherlands_2759794"
 ];
 var weatherDiv;
+getQueryVariable = function(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+};
 window.addEventListener('load', (event) => {
-  console.log('page is fully loaded');
+  console.log(getQueryVariable("full"));
   weatherDiv = document.getElementById("weather");
   weatherList.forEach(createWeatherFrame);
 });
