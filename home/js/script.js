4234d80870981ad32cd22511b38dacd9de8d84b7
Dirk Bogaert <dirk@bogaert.es>
2020-06-04 09:39:09 +0200

    Update script.js

--- home/js/script.js
+++ home/js/script.js
@@ -17,9 +17,9 @@ window.addEventListener('load', (event) => {
 });
 createWeatherFrame = function(link) {
   var newEl = document.createElement("iframe");
-  var mySource = "https://www.meteoblue.com/en/weather/widget/three/";
+  var mySource = "https://www.meteoblue.com/en/weather/widget/daily/";	  
   mySource += link;
-  mySource += "?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image";
+  mySource += "?geoloc=fixed&days=5&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&winddirection=1&uv=0&humidity=0&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&pressure=0&layout=light";
   newEl.classList.add("weatherFrame");
   newEl.src=mySource;
   weatherDiv.appendChild(newEl);
