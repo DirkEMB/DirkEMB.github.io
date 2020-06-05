1d8be7fc01df8270b2ff739b9dcd4608537a96f2
Dirk Bogaert <dirk@bogaert.es>
2020-06-05 09:40:31 +0200

    Update script.js

--- home/js/script.js
+++ home/js/script.js
@@ -28,7 +28,7 @@ window.addEventListener('load', (event) => {
 createWeatherFrame = function(link) {
   var newEl = document.createElement("iframe");
   var mySource = "https://www.meteoblue.com/en/weather/widget/";
-  if (!getQueryVariable("full")) {
+  if (getQueryVariable("full")) {
     mySource += "daily/;"
     mySource += link;
     mySource += "?geoloc=fixed&days=5&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&winddirection=1&uv=0&humidity=0&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&pressure=0&layout=light";
