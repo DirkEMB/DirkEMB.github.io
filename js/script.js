b64e7c0d9c960749a62ed44e37906574515ea2ac
Dirk Bogaert <dirk@bogaert.es>
2026-01-03 10:32:01 +0100

    Add vincent munier pics

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 window.addEventListener("load" , function () {
-    document.getElementById('fotos').innerHTML = 'test';
+    addPics (document.getElementById('fotos'));
     });
+
+const addPics = function(ctr) {
+    var myUrls = [];
+    for (i=1; i < 144; i++) {
+        myUrls.push(`https://www.vincentmunier.com/wp-content/uploads/2025/12/CDF${i.toString().padStart(3,'0')}.jpg`);
+    }
+    myUrls.forEach(my_pic_url => {
+       let myImg = document.createElement('img');
+       myImg.src = my_pic_url;
+       ctr.appendChild(myImg);
+    });
+};
