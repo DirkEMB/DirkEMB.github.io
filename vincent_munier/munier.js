4b1fdc1272c1726b86f3d5b8348d7058de3a0ccf
Dirk Bogaert <dirk@bogaert.es>
2026-01-05 10:11:06 +0100

    create vincent_munier

---
+++ vincent_munier/munier.js
@@ -0,0 +1,15 @@
+window.addEventListener("load" , function () {
+    // vincent munier pics: addPics (document.getElementById('fotos'));
+    });
+
+const addPics = function(ctr) { //vincent munier pics le chant des forets
+    var myUrls = [];
+    for (i=1; i < 144; i++) {
+        myUrls.push(`https://www.vincentmunier.com/wp-content/uploads/2025/12/CDF${i.toString().padStart(3,'0')}.jpg`);
+    }
+    myUrls.forEach(my_pic_url => {
+       let myImg = document.createElement('img');
+       myImg.src = my_pic_url;
+       ctr.appendChild(myImg);
+    });
+};
